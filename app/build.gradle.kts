c0ed747aa9d1b18c4957ffb6fe77206cb49f0f33
icylian <red4357@gmail.com>
2026-03-08 23:09:57 +0900

    bump version to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.sekusarisu.yanami"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
